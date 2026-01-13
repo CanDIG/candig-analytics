5041815f697f7704b1591d7ed10f8037cc6cd4d7
Marion <mshadbolt@users.noreply.github.com>
2026-01-13 15:45:03 -0800

    add initial

---
+++ mohccn-report/run_completeness_reporting.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import argparse
+import requests as rq
+import subprocess
+
+PSQL_USER="admin"
+
+
+# def parse_args():
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument('--token', type=str, required=True, help="token for the candig deployment you are retrieving data from.")
+#     parser.add_argument('--url', type=str, required=True, help="URL of the candig deployment you are retrieving data from")
+#     args = parser.parse_args()
+#     return args
+
+def main():
+    # args = parse_args()
+    with open("minimal_clinical_query.sql", "r") as f:
+        minimal_sql_q = f.read()
+    subprocess.run(["docker", "exec", "-i", "candigv2_postgres-db_1", "touch", "minimal_completeness.csv"])
+    result = subprocess.run(["cat", "./minimal_clinical_query.sql", "|", "docker", "exec", "-i",
+                             "candigv2_postgres-db_1", "psql", "-U", "admin", "-d", "clinical", "-c",
+                            f"{minimal_sql_q}", ">", "minimal_completeness.csv"])
+
+
+
+
+if __name__ == "__main__":
+    main()
