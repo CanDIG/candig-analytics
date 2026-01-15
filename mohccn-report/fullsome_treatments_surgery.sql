bcb9af1f0256fb61cd0175a4b4609ce92447acc0
Marion <mshadbolt@users.noreply.github.com>
2026-01-15 11:21:09 -0800

    fix

--- mohccn-report/fullsome_treatments_surgery.sql
+++ mohccn-report/fullsome_treatments_surgery.sql
@@ -2,7 +2,7 @@ COPY (SELECT mohpackets_donor.program_id_id, mohpackets_donor.submitter_donor_id
 mohpackets_treatment.submitter_treatment_id, treatment_type, surgery_site, surgery_location
 FROM mohpackets_donor LEFT JOIN mohpackets_primarydiagnosis ON mohpackets_donor.submitter_donor_id = mohpackets_primarydiagnosis.submitter_donor_id
 LEFT JOIN mohpackets_treatment ON mohpackets_primarydiagnosis.submitter_primary_diagnosis_id = mohpackets_treatment.submitter_primary_diagnosis_id
-RIGHT JOIN mohpackets_surgery ON mohpackets_specimen.submitter_treatment_id = mohpackets_surgery.submitter_treatment_id
+RIGHT JOIN mohpackets_surgery ON mohpackets_treatment.submitter_treatment_id = mohpackets_surgery.submitter_treatment_id
 WHERE mohpackets_donor.program_id_id IS NOT NULL
   AND treatment_type IS NOT NULL
   AND is_primary_treatment IS NOT NULL
