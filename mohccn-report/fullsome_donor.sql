2d050577229788d22e090f0a26fa28c61ab7eb77
Marion <mshadbolt@users.noreply.github.com>
2026-01-18 20:48:54 -0800

    missing comma

--- mohccn-report/fullsome_donor.sql
+++ mohccn-report/fullsome_donor.sql
@@ -4,7 +4,7 @@ WHERE gender IS NOT NULL
   AND sex_at_birth IS NOT NULL
   AND date_of_birth IS NOT NULL
   AND date_resolution IS NOT NULL) TO '/tmp/fullsome_donor_completeness.csv' with (FORMAT CSV, HEADER);
-COPY (SELECT program_id_id, submitter_donor_id COUNT(*)
+COPY (SELECT program_id_id, submitter_donor_id, COUNT(*)
   FROM mohpackets_donor
   GROUP BY program_id_id, submitter_donor_id)
   TO '/tmp/fullsome_donor_count.csv' with (FORMAT CSV, HEADER);
