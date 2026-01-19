c4b7de0e39b678e8b6f0a43aaff6e6bebbd3a686
Marion <mshadbolt@users.noreply.github.com>
2026-01-19 15:11:12 -0800

    bracket

--- mohccn-report/fullsome_sample.sql
+++ mohccn-report/fullsome_sample.sql
@@ -5,5 +5,5 @@ WHERE specimen_tissue_source IS NOT NULL
   AND specimen_type IS NOT NULL
   AND sample_type IS NOT NULL) TO '/tmp/fullsome_sample_completeness.csv' with (FORMAT CSV, HEADER);
 COPY (SELECT  program_id_id, submitter_donor_id, submitter_sample_id, COUNT(*)
-  FROM mohpackets_sampleregistration
+  FROM mohpackets_sampleregistration)
   TO '/tmp/fullsome_sample_count.csv' with (FORMAT CSV, HEADER);
