4aaacae10fd82f42f0b0fac829aa5a098dc861a6
Marion <mshadbolt@users.noreply.github.com>
2026-01-18 20:46:20 -0800

    more sql

--- mohccn-report/fullsome_specimen.sql
+++ mohccn-report/fullsome_specimen.sql
@@ -10,10 +10,10 @@ WHERE mohpackets_specimen.program_id_id IS NOT NULL
   AND specimen_tissue_source IS NOT NULL
   AND tumour_normal_designation IS NOT NULL
   AND specimen_type IS NOT NULL
-  AND sample_type IS NOT NULL) TO '/tmp/fullsome_specimen_sample_completeness.csv' with (FORMAT CSV, HEADER);
+  AND sample_type IS NOT NULL) TO '/tmp/fullsome_specimen_completeness.csv' with (FORMAT CSV, HEADER);
 COPY (SELECT  mohpackets_specimen.program_id_id, mohpackets_specimen.submitter_donor_id,
   mohpackets_specimen.submitter_specimen_id, COUNT(*)
   FROM mohpackets_specimen
   LEFT JOIN mohpackets_sampleregistration ON mohpackets_specimen.submitter_specimen_id = mohpackets_sampleregistration.submitter_specimen_id
   GROUP BY mohpackets_specimen.program_id_id, mohpackets_specimen.submitter_donor_id, mohpackets_specimen.submitter_specimen_id)
-  TO '/tmp/fullsome_specimen_sample_count.csv' with (FORMAT CSV, HEADER);
+  TO '/tmp/fullsome_specimen_count.csv' with (FORMAT CSV, HEADER);
